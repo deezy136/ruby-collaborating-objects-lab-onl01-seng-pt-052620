16f3f41ddc164fb93b10e9e213f1d0ab705e3431
Clyde Griffin <deezy13@gmail.com>
2020-06-16 23:16:29 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,7 +6,7 @@ class Artist
   def initialize(name)
     @name = name
     @songs = []
-  
+    save
   end
 
   def self.all
@@ -35,9 +35,12 @@ class Artist
     songs.each { |song| puts song.name }
   end
 
-  
+  def save
+    @@all << self
+  end
 
   def songs
     @songs
   end
 end 
+ 15  lib/mp3_importer.rb 
